c1f85b42d694a4409e8726535750b19e9497eceb
Khánh Hưng <khanhhungzm@gmail.com>
2026-04-23 10:50:19 +0700

    refactor: return camel case upload response

--- Program.cs
+++ Program.cs
@@ -2,11 +2,17 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Platform.ProductCoverUpload.Function.Configurations;
 using Platform.ProductCoverUpload.Function.Services;
+using System.Text.Json;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices((context, services) =>
     {
+        services.Configure<JsonSerializerOptions>(options =>
+        {
+            options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        });
+
         services
             .AddOptions<BlobStorageOptions>()
             .Bind(context.Configuration.GetSection(BlobStorageOptions.SectionName));
